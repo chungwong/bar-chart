dfe78b3b2591ef51d6346ecd6d93b6bbd2654737
Chung Wong <chung@sydneytools.com.au>
2024-07-12 09:20:45 +1000

    bug after 2288 is merged

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #![allow(non_snake_case)]
-mod bar_chart;
 
 use dioxus::prelude::*;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 use tracing::Level;
-
-use crate::bar_chart::{AsciiBarChart, BarChartExt};
 
 #[derive(Clone, Routable, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
 enum Route {
@@ -26,35 +24,37 @@ fn App() -> Element {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize)]
-pub struct SalesData {
-    model: String,
-    row_total: f64,
-}
-
-impl SalesData {
-    pub fn new(model: String, row_total: f64) -> Self {
-        Self { model, row_total }
-    }
-}
-
-impl BarChartExt for SalesData {
-    type X = f64;
-}
-
 #[component]
 fn Home() -> Element {
-    let data = Arc::new(vec![
-        SalesData::new("Clare".into(), 64.0),
-        SalesData::new("Donegal".into(), 48.0),
-        SalesData::new("Mayo".into(), 57.0),
-        SalesData::new("Meath".into(), 67.0),
-        SalesData::new("Offaly".into(), 58.0),
-        SalesData::new("Tipperary".into(), 59.0),
-        SalesData::new("Wicklow".into(), 74.0),
-    ]);
+    let global_filter: Signal<GlobalFilter> = use_signal(|| GlobalFilter::default());
 
     rsx! {
-        AsciiBarChart { items: data }
+        button {
+            onclick: move |_| {
+                let filter: GlobalFilter = global_filter.read_unchecked().to_owned();
+                async move {
+                    get_server_data(filter).await;
+                }
+            },
+            "Button"
+        }
     }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
+pub struct GlobalFilter {
+    pub test_string: String,
+
+    pub test_vec: Vec<String>,
+    pub test_hashset: HashSet<String>,
+    pub test_hashmap: HashMap<String, String>,
+    // wrap them in Option to bypass the bug
+    // pub test_vec: Option<Vec<String>>,
+    // pub test_hashset: Option<HashSet<String>>,
+    // pub test_hashmap: Option<HashMap<String, String>>,
+}
+
+#[server]
+async fn get_server_data(global_filter: GlobalFilter) -> Result<(), ServerFnError> {
+    Ok(())
 }
